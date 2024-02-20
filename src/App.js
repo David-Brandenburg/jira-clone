ba16746ccc2527d5dbab35eab1adad907ea9a16a
win/ubuntu <d-brandenburg@hotmail.de>
2024-02-20 09:29:44 +0100

    ready to merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { Ticket2 } from "./components/ticket/ticket copy.jsx";
 import { Ticket } from "./components/ticket/ticket.jsx";
 import { TicketBearbeiten } from "./components/ticket/ticketBearbeiten";
 import { TicketErstellen } from "./components/ticket/ticketErstellen";
@@ -6,7 +5,7 @@ import { TicketErstellen } from "./components/ticket/ticketErstellen";
 function App() {
   return (
     <>
-      <Ticket2 />
+      <Ticket />
     </>
   );
 }
--- src/components/ticket/ticket.jsx
+++ src/components/ticket/ticket.jsx
@@ -1,118 +1,280 @@
 import { useState, useEffect } from "react";
 import "./ticket.scss";
-import { TicketBearbeiten } from "./ticketBearbeiten";
+import { ToastContainer, toast } from "react-toastify";
 import { TicketErstellen } from "./ticketErstellen";
 
 export const Ticket = () => {
-  const [tickets, setTickets] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [selectedTicketId, setSelectedTicketId] = useState(null);
-  const [benutzer, setBenutzer] = useState("");
+  const [tickets, setTickets] = useState(null);
+  const [users, setUsers] = useState(null);
+  const [editTicket, setEditTicket] = useState(null);
   const [erstellen, setErstellen] = useState(false);
+  const [benutzer, setBenutzer] = useState("");
+  const [profile, setProfile] = useState("");
 
-  const handleEdit = (id) => {
-    setSelectedTicketId((prevId) => (prevId === id ? null : id));
+  const url = `http://localhost:5000/tickets`;
+  const url2 = `http://localhost:5000/users`;
+  const optionsGet = {
+    method: "GET",
   };
 
-  const updateCreator = async (id) => {
-    const userResponse = await fetch(`http://localhost:5000/users`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!userResponse.ok) {
-      throw new Error("Network response was not ok");
+  const fetchTickets = async () => {
+    try {
+      const response = await fetch(url, optionsGet);
+      if (!response.ok) {
+        throw new Error("Failed to fetch!", response.status);
+      }
+      const fetchedTickets = await response.json();
+      console.log("fetchedtickets:", fetchedTickets);
+      setTickets(fetchedTickets);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    const userData = await userResponse.json();
-    const selectedUser = userData.find((user) => user.id === benutzer);
-    if (selectedUser) {
-      const updatedUser = {
-        ...selectedUser,
-        ticketId: [...selectedUser.ticketId, id],
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch(url2, optionsGet);
+      if (!response.ok) {
+        throw new Error("Failed to fetch!", response.status);
+      }
+      const fetchedUsers = await response.json();
+      console.log("fetchedusers:", fetchedUsers);
+      setUsers(fetchedUsers);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleTicketEdit = async (ticketId) => {
+    try {
+      const response = await fetch(`${url}/${ticketId}`, optionsGet);
+      if (!response.ok) {
+        throw new Error("Failed to fetch ticket!", response.status);
+      }
+      const editTicket = await response.json();
+      console.log("Editable Ticket:", editTicket);
+      setEditTicket(editTicket);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSubmitEditedTicket = async (e) => {
+    e.preventDefault();
+    const editedTitle = e.target.title.value;
+    const editedDesc = e.target.description.value;
+    const editedStatus = e.target.status.value;
+    const editedDate = e.target.deadline.value;
+    const ticketId = document.getElementById("ticketId").innerText;
+    console.log(editedTitle, editedDesc, editedStatus, editedDate);
+    try {
+      const optionsPatch = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: editedTitle,
+          desc: editedDesc,
+          status: editedStatus,
+          deadline: editedDate,
+        }),
       };
+      const response = await fetch(`${url}/${ticketId}`, optionsPatch);
+      if (!response.ok) {
+        throw new Error("Failed to fetch!", response.status);
+      }
+      toast.success("Ticket erfolgreich bearbeitet!");
+      setEditTicket(null);
+      fetchTickets();
+    } catch (error) {
+      toast.error("Ticket konnte nicht gespeichert werden!");
+      console.error(error);
+    }
+    console.log(ticketId);
+  };
 
-      const updateUserResponse = await fetch(
-        `http://localhost:5000/users/${selectedUser.id}`,
-        {
+  const updateEditor = async (ticketId) => {
+    const selectedUser = users.find((user) => user.id === benutzer);
+    const ticket = tickets.find((ticket) => ticket.id === ticketId);
+
+    if (selectedUser && ticket) {
+      // Überprüfen, ob die Ticket-ID bereits im Benutzerprofil vorhanden ist
+      if (!selectedUser.ticketId.includes(ticketId)) {
+        const updatedUser = {
+          ...selectedUser,
+          ticketId: [...selectedUser.ticketId, ticketId], // Hinzufügen der neuen Ticket-ID
+        };
+
+        // Benutzerprofil aktualisieren
+        const updateUserResponse = await fetch(`${url2}/${selectedUser.id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(updatedUser),
-        }
-      );
+        });
 
-      if (!updateUserResponse.ok) {
-        throw new Error("Network response was not ok");
+        if (!updateUserResponse.ok) {
+          throw new Error("Network response was not ok");
+        }
+      } else {
+        console.log("Ticket ID already exists in user profile.");
       }
     }
   };
 
   useEffect(() => {
-    fetch("http://localhost:5000/tickets")
-      .then((response) => response.json())
-      .then((data) => {
-        setTickets(data);
-        setLoading(false);
-      })
-      .catch((error) => console.error(error));
+    fetchTickets();
+    fetchUsers();
   }, []);
-
-  const handleSubmit = (e, id) => {
-    e.preventDefault();
-    updateCreator(id);
-  };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
   const handelErstellen = () => {
     setErstellen(!erstellen);
   };
 
-  return (
-    <div className="tickets-container">
-      {tickets.map(function (item) {
-        return (
-          <div className="card" key={item.id}>
-            <div className="überschrift_id">
-              <h2>{item.title}</h2> <p>{item.id}</p> <p>{}</p>
-            </div>
-            <p>{item.desc}</p>
-            <h4>Status</h4>
-            <p>{item.status}</p>
-            <h4>Fällig am:</h4>
-            <p>{item.deadline}</p>
+  const handleUpdateEditor = async (e, ticketId) => {
+    e.preventDefault();
+    updateEditor(ticketId);
+    const selectedUser = users.find((user) => user.id === benutzer);
+    if (selectedUser) {
+      const optionsPatch = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          editor: selectedUser.fname,
+        }),
+      };
+      try {
+        const response = await fetch(`${url}/${ticketId}`, optionsPatch);
+        if (!response.ok) {
+          throw new Error("Failed to update editor!", response.status);
+        }
+        toast.success("Editor erfolgreich aktualisiert!");
+        fetchTickets(); // Tickets erneut abrufen, um die aktualisierten Daten anzuzeigen
+      } catch (error) {
+        toast.error("Editor konnte nicht aktualisiert werden!");
+        console.error(error);
+      }
+    }
+  };
 
-            <button
-              className="beabrbeiten-btn"
-              onClick={() => handleEdit(item.id)}>
-              {selectedTicketId === item.id ? "Abbrechen" : "Ticket Bearbeiten"}
-            </button>
-            {selectedTicketId === item.id && (
-              <div className="ticket-bearbeiten-container">
-                <TicketBearbeiten id={item.id} />
+  return (
+    <div className="main">
+      <ToastContainer />
+      <div className="ticket-container">
+        {tickets?.map((ticket) => (
+          <div className="ticket" key={ticket.id}>
+            <div className="ticket-heading">
+              <h2>{ticket.title}</h2>
+              <p>{ticket.id}</p>
+              <p>{ticket.creator}</p>
+              <p>{ticket.editor}</p>
+            </div>
+            <div className="ticket-description">
+              <p>{ticket.desc}</p>
+            </div>
+            <div className="ticket-bottom">
+              <div className="ticket-bottom-item">
+                <h4>Status</h4>
+                <p>{ticket.status}</p>
               </div>
-            )}
+              <div className="ticket-bottom-item">
+                <h4>Fällig am:</h4>
+                <p>{ticket.deadline}</p>
+              </div>
+            </div>
+            <div className="ticket-buttons">
+              <button
+                className="btn edit-btn"
+                onClick={() => handleTicketEdit(ticket.id)}>
+                Bearbeiten
+              </button>
+            </div>
             <h2>Benutzer zuordnen:</h2>
-            <select
-              value={benutzer}
-              onChange={(e) => setBenutzer(e.target.value)}>
-              <option value=""></option>
-              <option value="1">danny</option>
-              <option value="2">michelle</option>
-              <option value="3">david</option>
-            </select>
-            <button onClick={(e) => handleSubmit(e, item.id)}>
-              Update Benutzer
-            </button>
+            <div>
+              <select
+                value={benutzer}
+                onChange={(e) => setBenutzer(e.target.value)}>
+                <option></option>
+                {users?.map((user, index) => (
+                  <option key={index} value={user.id}>
+                    {user.fname}
+                  </option>
+                ))}
+              </select>
+              <button onClick={(e) => handleUpdateEditor(e, ticket.id)}>
+                Update Benutzer
+              </button>
+            </div>
           </div>
-        );
-      })}
+        ))}
+      </div>
+      {editTicket && (
+        <div
+          className="blocker"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}>
+          <form
+            className="edit-ticket-modal"
+            onSubmit={(e) => {
+              handleSubmitEditedTicket(e, editTicket.id);
+            }}>
+            <div className="ticket-heading">
+              <input
+                type="text"
+                name="title"
+                id="title"
+                defaultValue={editTicket.title}
+              />
+              <p id="ticketId">{editTicket.id}</p>
+              <p>{editTicket.creator}</p>
+            </div>
+            <div className="ticket-description">
+              <textarea
+                name="description"
+                id="description"
+                defaultValue={editTicket.desc}
+              />
+            </div>
+            <div className="ticket-bottom">
+              <div className="ticket-bottom-item">
+                <h4>Status</h4>
+                <select name="status" defaultValue={editTicket.status}>
+                  <option value="Fertig">Fertig</option>
+                  <option value="In Progress">In Progress</option>
+                  <option value="Nicht zugewiesen">Nicht zugewiesen</option>
+                </select>
+              </div>
+              <div className="ticket-bottom-item">
+                <h4>Fällig am:</h4>
+                <input
+                  type="date"
+                  name="deadline"
+                  id="deadline"
+                  defaultValue={editTicket.deadline}
+                />
+              </div>
+            </div>
+            <div className="ticket-buttons">
+              <button type="submit" className="btn save-btn">
+                Speichern
+              </button>
+              <button
+                className="btn cancel-btn"
+                onClick={() => {
+                  setEditTicket(null);
+                  toast.warn("Ticket bearbeiten abgebrochen!");
+                }}>
+                Abbrechen
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
       <div className="card2">
         <button
           className="erstellen-btn"
